051724b8421a9d2b3c9d1afac9a3a34d3a52e818
Cyfine <58298819+Cyfine@users.noreply.github.com>
2022-04-15 23:01:12 +0800

    add perlin noise for tree generation

--- js/lib/util.js
+++ js/lib/util.js
@@ -19,21 +19,53 @@ function mapVal(val, inMin, inMax, outMin, outMax) {
  *
  * @returns {number}
  */
-function getTypeFromDistribution() {
-
-    let rand = Math.random();
-
-    for (let i = 0, left =0; i < arguments.length; i++) {
-        let right = left + arguments[i];
-        if (rand >= left && rand < right) {
-            return i;
-        }else{
-            left = right;
-        }
-
+function drawTypeFromDistribution(weight) {
+    let sum = 0;
+    for (let i = 0; i < weight.length; i++) {
+        sum += weight[i];
     }
 
+    let  distribution = []
+    for (let i = 0; i < weight.length; i++) {
+        distribution.push(weight[i]/sum);
+    }
+
+    let rand = Math.random();
+    for (let i = 0, left = 0; i < distribution.length; i++) {
+        let right = left + distribution[i];
+        if (rand >= left && rand < right) {
+            return i;
+        } else {
+            left = right;
+        }
+    }
 }
+
+function fallInRange(val, weight){
+    let sum = 0;
+    for (let i = 0; i < weight.length; i++) {
+        sum += weight[i];
+    }
+
+    let  distribution = []
+    for (let i = 0; i < weight.length; i++) {
+        distribution.push(weight[i]/sum);
+    }
+
+    for (let i = 0, left = 0; i < distribution.length; i++) {
+        let right = left + distribution[i];
+        if (val >= left && val < right) {
+            return i;
+        } else {
+            left = right;
+        }
+    }
+}
+
+
+
+
+
 
 
 
