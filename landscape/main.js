f412485a67494ec69ec2484a924a57b0fd331166
Cyfine <58298819+Cyfine@users.noreply.github.com>
2022-04-16 17:34:36 +0800

    implement the plane control

--- landscape/main.js
+++ landscape/main.js
@@ -71,6 +71,7 @@ function init() {
     testChunk();
     createAirPlane();
 
+
     document.addEventListener('mousemove', handleMousemove, false);
 
     loop();
@@ -308,13 +309,12 @@ let FiveConesTree = function (color) {
 }
 
 
-function loop() {
-    controls.update();
-
-    // render the scene
+function updateAirPlaneControl() {
     const delta = clock.getDelta() + 0.01;
     const moveDistance = 30 * delta;
     const rotateAngle = Math.PI / 2 * delta;
+    airPlane.updatePropeller();
+    airPlane.mesh.translateZ(-moveDistance);
 
     // airPlane.mesh.rotateY(Math.PI/2);
     if (keyboard.pressed("down")) {
@@ -342,21 +342,27 @@ function loop() {
     const relativeCameraOffset = new THREE.Vector3(0, 50, 200);
 
 
-    const rotationMat = new THREE.Matrix4();
-    rotationMat.set(
-        Math.cos(Math.PI/2), 0, -Math.sin(Math.PI/2), 0,
-        0, 1, 0, 0,
-        Math.sin(Math.PI/2), 0, Math.cos(Math.PI/2), 0,
-        0, 0, 0, 1
-    );
+    // const rotationMat = new THREE.Matrix4();
+    // rotationMat.set(
+    //     Math.cos(Math.PI/2), 0, -Math.sin(Math.PI/2), 0,
+    //     0, 1, 0, 0,
+    //     Math.sin(Math.PI/2), 0, Math.cos(Math.PI/2), 0,
+    //     0, 0, 0, 1
+    // );
 
 
     const cameraOffset = relativeCameraOffset.applyMatrix4(airPlane.mesh.matrixWorld);
 
-    camera.position.x = cameraOffset.x;
-    camera.position.y = cameraOffset.y;
-    camera.position.z = cameraOffset.z;
+
     controls.target = airPlane.mesh.position;
+
+}
+
+function loop() {
+    controls.update();
+
+    // render the scene
+    updateAirPlaneControl();
 
 
     renderer.render(scene, camera);
@@ -476,8 +482,8 @@ class AirPlane {
 
         // Cabin
 
-        var geomCabin = new THREE.BoxGeometry(80, 50, 50, 1, 1, 1);
-        var matCabin = new THREE.MeshPhongMaterial({color: Colors.red, shading: THREE.FlatShading});
+        var geomCabin = new THREE.BoxGeometry(50, 50, 80, 1, 1, 1);
+        var matCabin = new THREE.MeshPhongMaterial({color: Colors.blue, shading: THREE.FlatShading});
 
 
 
@@ -489,27 +495,27 @@ class AirPlane {
 
         // Engine
 
-        var geomEngine = new THREE.BoxGeometry(20, 50, 50, 1, 1, 1);
+        var geomEngine = new THREE.BoxGeometry(50, 50, 20, 1, 1, 1);
         var matEngine = new THREE.MeshPhongMaterial({color: Colors.white, shading: THREE.FlatShading});
         var engine = new THREE.Mesh(geomEngine, matEngine);
-        engine.position.x = 50;
+        engine.position.z = -50;
         engine.castShadow = true;
         engine.receiveShadow = true;
         this.mesh.add(engine);
 
         // Tail Plane
 
-        var geomTailPlane = new THREE.BoxGeometry(15, 20, 5, 1, 1, 1);
+        var geomTailPlane = new THREE.BoxGeometry(5, 20, 15, 1, 1, 1);
         var matTailPlane = new THREE.MeshPhongMaterial({color: Colors.red, shading: THREE.FlatShading});
         var tailPlane = new THREE.Mesh(geomTailPlane, matTailPlane);
-        tailPlane.position.set(-40, 20, 0);
+        tailPlane.position.set(0, 20, 40);
         tailPlane.castShadow = true;
         tailPlane.receiveShadow = true;
         this.mesh.add(tailPlane);
 
         // Wings
 
-        var geomSideWing = new THREE.BoxGeometry(30, 5, 120, 1, 1, 1);
+        var geomSideWing = new THREE.BoxGeometry(120, 5, 30, 1, 1, 1);
         var matSideWing = new THREE.MeshPhongMaterial({color: Colors.red, shading: THREE.FlatShading});
         var sideWing = new THREE.Mesh(geomSideWing, matSideWing);
         sideWing.position.set(0, 15, 0);
@@ -517,7 +523,7 @@ class AirPlane {
         sideWing.receiveShadow = true;
         this.mesh.add(sideWing);
 
-        var geomWindshield = new THREE.BoxGeometry(3, 15, 20, 1, 1, 1);
+        var geomWindshield = new THREE.BoxGeometry(20, 15, 3, 1, 1, 1);
         var matWindshield = new THREE.MeshPhongMaterial({
             color: Colors.white,
             transparent: true,
@@ -526,14 +532,14 @@ class AirPlane {
         });
         ;
         var windshield = new THREE.Mesh(geomWindshield, matWindshield);
-        windshield.position.set(5, 27, 0);
+        windshield.position.set(5, 27, -5);
 
         windshield.castShadow = true;
         windshield.receiveShadow = true;
 
         this.mesh.add(windshield);
 
-        var geomPropeller = new THREE.BoxGeometry(20, 10, 10, 1, 1, 1);
+        var geomPropeller = new THREE.BoxGeometry(10, 10, 20, 1, 1, 1);
 
 
         var matPropeller = new THREE.MeshPhongMaterial({color: Colors.brown, shading: THREE.FlatShading});
@@ -542,37 +548,37 @@ class AirPlane {
         this.propeller.castShadow = true;
         this.propeller.receiveShadow = true;
 
-        var geomBlade = new THREE.BoxGeometry(1, 80, 10, 1, 1, 1);
+        var geomBlade = new THREE.BoxGeometry(10, 80, 1, 1, 1, 1);
         var matBlade = new THREE.MeshPhongMaterial({color: Colors.brownDark, shading: THREE.FlatShading});
         var blade1 = new THREE.Mesh(geomBlade, matBlade);
-        blade1.position.set(8, 0, 0);
+        blade1.position.set(0, 0, -8);
 
         blade1.castShadow = true;
         blade1.receiveShadow = true;
 
         var blade2 = blade1.clone();
-        blade2.rotation.x = Math.PI / 2;
+        blade2.rotation.z = Math.PI / 2;
 
         blade2.castShadow = true;
         blade2.receiveShadow = true;
 
         this.propeller.add(blade1);
         this.propeller.add(blade2);
-        this.propeller.position.set(60, 0, 0);
+        this.propeller.position.set(0, 0, -60);
         this.mesh.add(this.propeller);
 
-        var wheelProtecGeom = new THREE.BoxGeometry(30, 15, 10, 1, 1, 1);
+        var wheelProtecGeom = new THREE.BoxGeometry(10, 15, 30, 1, 1, 1);
         var wheelProtecMat = new THREE.MeshPhongMaterial({color: Colors.red, shading: THREE.FlatShading});
         var wheelProtecR = new THREE.Mesh(wheelProtecGeom, wheelProtecMat);
-        wheelProtecR.position.set(25, -20, 25);
+        wheelProtecR.position.set(25, -20, -25);
         this.mesh.add(wheelProtecR);
 
-        var wheelTireGeom = new THREE.BoxGeometry(24, 24, 4);
+        var wheelTireGeom = new THREE.BoxGeometry(4, 24, 24);
         var wheelTireMat = new THREE.MeshPhongMaterial({color: Colors.brownDark, shading: THREE.FlatShading});
         var wheelTireR = new THREE.Mesh(wheelTireGeom, wheelTireMat);
-        wheelTireR.position.set(25, -28, 25);
+        wheelTireR.position.set(25, -28, -25);
 
-        var wheelAxisGeom = new THREE.BoxGeometry(10, 10, 6);
+        var wheelAxisGeom = new THREE.BoxGeometry(6, 10, 10);
         var wheelAxisMat = new THREE.MeshPhongMaterial({color: Colors.brown, shading: THREE.FlatShading});
         var wheelAxis = new THREE.Mesh(wheelAxisGeom, wheelAxisMat);
         wheelTireR.add(wheelAxis);
@@ -580,24 +586,24 @@ class AirPlane {
         this.mesh.add(wheelTireR);
 
         var wheelProtecL = wheelProtecR.clone();
-        wheelProtecL.position.z = -wheelProtecR.position.z;
+        wheelProtecL.position.x = -wheelProtecR.position.x;
         this.mesh.add(wheelProtecL);
 
         var wheelTireL = wheelTireR.clone();
-        wheelTireL.position.z = -wheelTireR.position.z;
+        wheelTireL.position.x = -wheelTireR.position.x;
         this.mesh.add(wheelTireL);
 
         var wheelTireB = wheelTireR.clone();
         wheelTireB.scale.set(.5, .5, .5);
-        wheelTireB.position.set(-35, -5, 0);
+        wheelTireB.position.set(0, -25, 35);
         this.mesh.add(wheelTireB);
 
         var suspensionGeom = new THREE.BoxGeometry(4, 20, 4);
         suspensionGeom.applyMatrix(new THREE.Matrix4().makeTranslation(0, 10, 0))
         var suspensionMat = new THREE.MeshPhongMaterial({color: Colors.red, shading: THREE.FlatShading});
         var suspension = new THREE.Mesh(suspensionGeom, suspensionMat);
-        suspension.position.set(-35, -5, 0);
-        suspension.rotation.z = -.3;
+        suspension.position.set(0, -5, 35);
+        suspension.rotation.x = -.3;
         this.mesh.add(suspension);
 
         // this.mesh.rotation.y = Math.PI/2;
@@ -607,6 +613,10 @@ class AirPlane {
         this.mesh.receiveShadow = true;
 
 
+    }
+
+    updatePropeller(){
+        this.propeller.rotation.z += 0.3;
     }
 
 
