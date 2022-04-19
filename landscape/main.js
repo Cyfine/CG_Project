356705def83013df70c3dae9e0cef749f628a64d
Xinyue-HU <19250797@life.hkbu.edu.hk>
2022-04-19 15:35:21 +0800

    fireBall function

--- landscape/main.js
+++ landscape/main.js
@@ -41,51 +41,6 @@ let waterMaterial, waterMesh;
 //     roughnessMap: waterRoughness, roughness: 0,
 //     aoMap: waterAmbientOcclusion
 // });
-const _VS = `uniform float time;
-varying float yPosition;
-void main(){
-    float x = position.x;
-    float y = position.y;
-    float PI = 3.141592653589;
-
-    float sx = 0.0;
-    float sy = 0.0;
-    float sz = 0.0;
-
-    float ti = 0.0;
-    float index = 1.0;
-    vec2 dir;//水波方向
-    for(int i = 0;i<3;i++){
-        ti = ti + 0.0005;
-        index +=1.0;
-        if(mod(index,2.0)==0.0){
-            dir = vec2(1.0,ti);
-        }else{
-            dir = vec2(-1.0,ti);
-        }
-        float l1 = 2.0 * PI / (0.5 + ti);//波长
-        float s1 = 20.0 * 2.0 / l1;//速度
-        float x1 = 1.0 * dir.x * sin(dot(normalize(dir),vec2(x,y)) * l1 + time * s1);
-        float y1 = 1.0 * dir.y * sin(dot(normalize(dir),vec2(x,y)) * l1 + time * s1);
-        float z1 = 1.0 * sin(dot(normalize(dir),vec2(x,y)) * l1 + time * s1);
-        sx +=x1;
-        sy +=y1;
-        sz +=z1;
-    }
-    sx = x + sx;
-    sy = y + sy;
-    yPosition = sz;
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(sx,sy,sin(sz) * 10.0,1.0);
-}`;
-//
-const _FS = `
-varying float yPosition;
-void main()
-{
-  vec3 white = vec3(255.,255.,255.);
-  vec3 blue = vec3(90./255.,160./255.,248./255.);
-  gl_FragColor = vec4(vec3(blue+0.0001*yPosition*white), 1);
-}`;
 
 //==================== airplane control ==========================
 let airPlane;
@@ -261,7 +216,7 @@ function handlePlayAudio(){
 }
 
 function updateMusic(musicName){
-    if (musicFlag===false&&backgroundSound!==null) backgroundSound.stop();
+    if (backgroundSound.isPlaying) backgroundSound.stop();
     // instantiate a loader
     const audioLoader = new THREE.AudioLoader();
     // instantiate audio object
@@ -433,6 +388,16 @@ let FiveConesTree = function (color) {
     s.position.y -= 130;
     s.rotation.x = Math.PI / 2;
     this.mesh.add(s);
+}
+
+let FireBall = function (color) {
+    this.mesh = new THREE.Object3D();
+    let ball = new THREE.SphereGeometry(50);
+    let material = new THREE.ShaderMaterial({
+        uniforms: {},
+        vertexShader: _fireballVS, fragmentShader: _fireballFS,
+    })
+    this.mesh.add(new THREE.Mesh(ball, material));
 }
 
 
@@ -685,6 +650,7 @@ class Chunk {
 
         this.createTerrain(false);
         this.decorateTree();
+        this.createFireBall();
         // this.createWater2();
         this.createWater(); //using shader
         this.createWater3(); //using setY
@@ -744,7 +710,9 @@ class Chunk {
 
 
     }
+    createFireBall(){
 
+    }
 
     decorateTree() {
         let treeAmt = 0;
@@ -850,6 +818,8 @@ class Chunk {
 
     //let waterMesh, waterMaterial, waterPlane;
     createWater() {
+        const xT = this.x * this.size + this.size / 2; //translate
+        const zT = this.z * this.size + this.size / 2; //translate
         this.waterPlane = new THREE.PlaneGeometry(this.size, this.size, this.segments / 4, this.segments / 4);
         this.waterMaterial = new THREE.ShaderMaterial({
             uniforms: {
@@ -1206,15 +1176,15 @@ class Weather {
     startCherry(density) {
         cherryCount = density;
         const shape = new THREE.Shape();
-        const x = -1;
-        const y = -2;
-        shape.moveTo(x + 1, y + 1);
-        shape.bezierCurveTo(x + 1, y + 1, x + 0.8, y, x, y);
-        shape.bezierCurveTo(x - 1.2, y, x - 1.2, y + 1.4, x - 1.2, y + 1.4);
-        shape.bezierCurveTo(x - 1.2, y + 2.2, x - 0.6, y + 3.08, x + 1, y + 3.8);
-        shape.bezierCurveTo(x + 2.4, y + 3.08, x + 8, y + 4.5, x + 3.2, y + 3.5);
-        shape.bezierCurveTo(x + 1.4, y + 1.4, x + 8, y, x + 5, y);
-        shape.bezierCurveTo(x + 1.4, y, x + 1, y + 1, x + 1, y + 1);
+        const x = -2.5;
+        const y = -5;
+        shape.moveTo(x + 2.5, y + 2.5);
+        shape.bezierCurveTo(x + 2.5, y + 2.5, x + 2, y, x, y);
+        shape.bezierCurveTo(x - 3, y, x - 3, y + 3.5, x - 3, y + 3.5);
+        shape.bezierCurveTo(x - 3, y + 5.5, x - 1.5, y + 7.7, x + 2.5, y + 9.5);
+        shape.bezierCurveTo(x + 6, y + 7.7, x + 8, y + 4.5, x + 8, y + 3.5);
+        shape.bezierCurveTo(x + 8, y + 3.5, x + 8, y, x + 5, y);
+        shape.bezierCurveTo(x + 3.5, y, x + 2.5, y + 2.5, x + 2.5, y + 2.5);
 
         const extrudeSettings = {
             steps: 2,
@@ -1239,12 +1209,15 @@ class Weather {
         scene.add(cherry);
         cherryArray = cherry.children;
     }
-    updateCherry(){this.updateParticle(cherry, cherryArray);}
+    updateCherry(){this.updateParticle(cherry, cherryArray, 1);}
     endCherry(){if(cherryCount>0) cherryCount-=20; else scene.remove(cherry);}
 
     startRain(density){
         rainCount = density;
-        rainGeometry = new THREE.SphereGeometry(1.5); // radius
+        const radius = 0.8;
+        const height = 2;
+        const segments = 16;
+        rainGeometry = new THREE.ConeBufferGeometry(radius, height, segments); // radius
         rainMaterial = new THREE.MeshBasicMaterial({color: Colors.blue});
         rain = new THREE.Group();
         for (let i = 0; i < rainCount; i++) {
@@ -1254,18 +1227,25 @@ class Weather {
                 (Math.random() - 0.5) * 4000,
                 (Math.random() - 0.5) * 2000
             );
+            rainMesh.rotation.set(0,0,Math.PI);
             rain.add(rainMesh);
         }
         scene.add(rain);
         rainArray = rain.children;
     }
-    updateRain(){this.updateParticle(rain, rainArray);}
+    updateRain(){this.updateParticle(rain, rainArray, 0.15);}
     endRain(){if(rainCount>0) rainCount-=20; else scene.remove(rain);}
 
     startLeaves(density){
         leavesCount = density;
-        leavesGeometry = new THREE.BoxGeometry(1.5,1.5,1.5); // radius
-        leavesMaterial = new THREE.MeshBasicMaterial({color: 0xFB8806,});
+        const points = [];
+        for (let i = 0; i < 10; ++i) {
+            points.push(new THREE.Vector2(Math.sin(i * 0.2) * 3 + 3, (i - 5) * .8));
+        }
+        leavesGeometry = new THREE.LatheBufferGeometry(points);
+        // const radius = 3;
+        // leavesGeometry =  new THREE.IcosahedronBufferGeometry(radius);// 20 sides，
+        leavesMaterial = new THREE.MeshNormalMaterial(); //fruit, color: 0xFB8806
         leaves = new THREE.Group();
         for (let i = 0; i < leavesCount; i++) {
             leavesMesh = new THREE.Mesh(leavesGeometry, leavesMaterial);
@@ -1279,7 +1259,7 @@ class Weather {
         scene.add(leaves);
         leavesArray = leaves.children;
     }
-    updateLeaves(){this.updateParticle(leaves, leavesArray);}
+    updateLeaves(){this.updateParticle(leaves, leavesArray, 1);}
     endLeaves(){if(leavesCount>0) leavesCount-=20; else scene.remove(leaves);}
 
     startSnow(density){
@@ -1301,23 +1281,23 @@ class Weather {
         scene.add(snow);
         flakeArray = snow.children;
     }
-    updateSnow(){this.updateParticle(snow, flakeArray);}
+    updateSnow(){this.updateParticle(snow, flakeArray, 1);}
     endSnow(){if(flakeCount>0) flakeCount-=20; else scene.remove(snow);}
 
-    updateParticle(particle, array){
+    updateParticle(particle, array, rotationSpeed){
         for (let i = 0; i < array.length / 2; i++) {
-            array[i].rotation.y += 0.01;
-            array[i].rotation.x += 0.02;
-            array[i].rotation.z += 0.03;
+            array[i].rotation.x += 0.02 * rotationSpeed;
+            array[i].rotation.z += 0.03 * rotationSpeed;
+            array[i].rotation.y += 0.01 * rotationSpeed;
             array[i].position.y -= 0.9;
             if (array[i].position.y < 0) {
                 array[i].position.y += 2000;
             }
         }
         for (let i = array.length / 2; i < array.length; i++) {
-            array[i].rotation.y -= 0.03;
-            array[i].rotation.x -= 0.03;
-            array[i].rotation.z -= 0.02;
+            array[i].rotation.x -= 0.03 * rotationSpeed;
+            array[i].rotation.z -= 0.02 * rotationSpeed;
+            array[i].rotation.y -= 0.03 * rotationSpeed;
             array[i].position.y -= 0.8;
             if (array[i].position.y < -0) {
                 array[i].position.y += 2000;
@@ -1349,13 +1329,13 @@ class Weather {
 
     switchSeason(){
         if(cnt%4===0) {
-            season = Spring; this.startCherry(5000); scene.remove(snow);
+            season = Spring; this.startCherry(500); scene.remove(snow);
             if(backgroundSound!==undefined) updateMusic("./music/Spring.mp3");
         }else if(cnt%4===1) {
             season = Summer; this.startRain(5000); scene.remove(cherry);
             if(backgroundSound!==undefined) updateMusic("./music/Summer.mp3");
         }else if(cnt%4===2) {
-            season = Fall; this.startLeaves(5000); scene.remove(rain);
+            season = Fall; this.startLeaves(500); scene.remove(rain);
             if(backgroundSound!==undefined) updateMusic("./music/Fall.mp3");
         }else {
             season = Winter; this.startSnow(5000); scene.remove(leaves);
@@ -1364,3 +1344,247 @@ class Weather {
         cnt++;
     }
 }
+//==================== shaders ==========================
+const _VS = `uniform float time;
+varying float yPosition;
+void main(){
+    float x = position.x;
+    float y = position.y;
+    float PI = 3.141592653589;
+
+    float sx = 0.0;
+    float sy = 0.0;
+    float sz = 0.0;
+
+    float ti = 0.0;
+    float index = 1.0;
+    vec2 dir;//水波方向
+    for(int i = 0;i<3;i++){
+        ti = ti + 0.0005;
+        index +=1.0;
+        if(mod(index,2.0)==0.0){
+            dir = vec2(1.0,ti);
+        }else{
+            dir = vec2(-1.0,ti);
+        }
+        float l1 = 2.0 * PI / (0.5 + ti);//波长
+        float s1 = 20.0 * 2.0 / l1;//速度
+        float x1 = 1.0 * dir.x * sin(dot(normalize(dir),vec2(x,y)) * l1 + time * s1);
+        float y1 = 1.0 * dir.y * sin(dot(normalize(dir),vec2(x,y)) * l1 + time * s1);
+        float z1 = 1.0 * sin(dot(normalize(dir),vec2(x,y)) * l1 + time * s1);
+        sx +=x1;
+        sy +=y1;
+        sz +=z1;
+    }
+    sx = x + sx;
+    sy = y + sy;
+    yPosition = sz;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(sx,sy,sin(sz) * 10.0,1.0);
+}`;
+const _FS = `
+varying float yPosition;
+void main()
+{
+  vec3 white = vec3(255.,255.,255.);
+  vec3 blue = vec3(90./255.,160./255.,248./255.);
+  gl_FragColor = vec4(vec3(blue+0.0001*yPosition*white), 1);
+}`;
+const _fireballVS = `
+attribute vec3 position;
+attribute vec3 normal;
+uniform mat3 normalMatrix;
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+uniform mat4 modelMatrix;
+uniform mat4 viewMatrix;
+uniform float time;
+varying vec3 fNormal;
+varying vec3 fPosition;
+varying vec3 color;
+
+vec3 mod289(vec3 x)
+{
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec4 mod289(vec4 x)
+{
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec4 permute(vec4 x)
+{
+  return mod289(((x*34.0)+1.0)*x);
+}
+
+vec4 taylorInvSqrt(vec4 r)
+{
+  return 1.79284291400159 - 0.85373472095314 * r;
+}
+
+vec3 fade(vec3 t) {
+  return t*t*t*(t*(t*6.0-15.0)+10.0);
+}
+
+// Classic Perlin noise
+float cnoise(vec3 P)
+{
+  vec3 Pi0 = floor(P); // Integer part for indexing
+  vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1
+  Pi0 = mod289(Pi0);
+  Pi1 = mod289(Pi1);
+  vec3 Pf0 = fract(P); // Fractional part for interpolation
+  vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+  vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+  vec4 iy = vec4(Pi0.yy, Pi1.yy);
+  vec4 iz0 = Pi0.zzzz;
+  vec4 iz1 = Pi1.zzzz;
+  vec4 ixy = permute(permute(ix) + iy);
+  vec4 ixy0 = permute(ixy + iz0);
+  vec4 ixy1 = permute(ixy + iz1);
+  vec4 gx0 = ixy0 * (1.0 / 7.0);
+  vec4 gy0 = fract(floor(gx0) * (1.0 / 7.0)) - 0.5;
+  gx0 = fract(gx0);
+  vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+  vec4 sz0 = step(gz0, vec4(0.0));
+  gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+  gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+  vec4 gx1 = ixy1 * (1.0 / 7.0);
+  vec4 gy1 = fract(floor(gx1) * (1.0 / 7.0)) - 0.5;
+  gx1 = fract(gx1);
+  vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+  vec4 sz1 = step(gz1, vec4(0.0));
+  gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+  gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+  vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+  vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+  vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+  vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+  vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+  vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+  vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+  vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+  vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+  g000 *= norm0.x;
+  g010 *= norm0.y;
+  g100 *= norm0.z;
+  g110 *= norm0.w;
+  vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+  g001 *= norm1.x;
+  g011 *= norm1.y;
+  g101 *= norm1.z;
+  g111 *= norm1.w;
+  float n000 = dot(g000, Pf0);
+  float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+  float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+  float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+  float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+  float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+  float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+  float n111 = dot(g111, Pf1);
+  vec3 fade_xyz = fade(Pf0);
+  vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+  vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+  float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x);
+  return 2.2 * n_xyz;
+}
+
+// Classic Perlin noise, periodic variant
+float pnoise(vec3 P, vec3 rep)
+{
+  vec3 Pi0 = mod(floor(P), rep); // Integer part, modulo period
+  vec3 Pi1 = mod(Pi0 + vec3(1.0), rep); // Integer part + 1, mod period
+  Pi0 = mod289(Pi0);
+  Pi1 = mod289(Pi1);
+  vec3 Pf0 = fract(P); // Fractional part for interpolation
+  vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+  vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+  vec4 iy = vec4(Pi0.yy, Pi1.yy);
+  vec4 iz0 = Pi0.zzzz;
+  vec4 iz1 = Pi1.zzzz;
+  vec4 ixy = permute(permute(ix) + iy);
+  vec4 ixy0 = permute(ixy + iz0);
+  vec4 ixy1 = permute(ixy + iz1);
+  vec4 gx0 = ixy0 * (1.0 / 7.0);
+  vec4 gy0 = fract(floor(gx0) * (1.0 / 7.0)) - 0.5;
+  gx0 = fract(gx0);
+  vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+  vec4 sz0 = step(gz0, vec4(0.0));
+  gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+  gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+  vec4 gx1 = ixy1 * (1.0 / 7.0);
+  vec4 gy1 = fract(floor(gx1) * (1.0 / 7.0)) - 0.5;
+  gx1 = fract(gx1);
+  vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+  vec4 sz1 = step(gz1, vec4(0.0));
+  gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+  gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+  vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+  vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+  vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+  vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+  vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+  vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+  vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+  vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+  vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+  g000 *= norm0.x;
+  g010 *= norm0.y;
+  g100 *= norm0.z;
+  g110 *= norm0.w;
+  vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+  g001 *= norm1.x;
+  g011 *= norm1.y;
+  g101 *= norm1.z;
+  g111 *= norm1.w;
+  float n000 = dot(g000, Pf0);
+  float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+  float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+  float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+  float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+  float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+  float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+  float n111 = dot(g111, Pf1);
+  vec3 fade_xyz = fade(Pf0);
+  vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+  vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+  float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x);
+  return 2.2 * n_xyz;
+}
+
+float map(float value, float low1, float high1, float low2, float high2) {
+  return low2 + (value - low1) * (high2 - low2) / (high1 - low1);
+}
+
+void main()
+{
+  // fNormal = normalize(normalMatrix * normal);
+  
+  fNormal = normal;
+  
+  float region = 2.5;
+  float speed = 15.0;
+  
+  float displacement = 1.0 * cnoise(position * region - time * speed);
+  
+  color = 2.0 * vec3(map(displacement, 0.5 , 0.78, 0.3 , 0.0), map(displacement, 0.0, 0.5, 0.3, 0.0), map(displacement, 0.0, 0.3, 0.0 , 0.0));
+
+  vec3 newPosition = position + normal * displacement * 0.5;
+  
+  vec4 pos = modelViewMatrix * vec4(newPosition, 1.0);
+  fPosition = pos.xyz;
+  gl_Position = projectionMatrix * pos;
+}`;
+const _fireballFS = `
+precision highp float;
+uniform float time;
+uniform vec2 resolution;
+varying vec3 fPosition;
+varying vec3 fNormal;
+varying vec3 color;
+
+void main()
+{
+  gl_FragColor = vec4(color, 1.0);
+}
+`
