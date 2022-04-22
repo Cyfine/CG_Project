c128feac51e4e824895bfc3e5cb73392eb9c2a45
Cyfine <58298819+Cyfine@users.noreply.github.com>
2022-04-22 10:54:09 +0800

    fix bug

--- landscape/main.js
+++ landscape/main.js
@@ -109,9 +109,9 @@ function resetGame() {
         point: 0,
         clock: new THREE.Clock(),
         energyLastTime: 1000,
-        energyGenerateSpeed: 10,
+        energyGenerateSpeed: 1, 
         energyGeneratePossibility: 0.1,
-        energyLost: 0.1,
+        energyLost: 0.05,
         score: 0,
         bluePoints: 10,
         goldenPoints: 100,
@@ -297,6 +297,7 @@ function createLights() {
     // the first parameter is the sky color, the second parameter is the ground color,
     // the third parameter is the intensity of the light
     hemisphereLight = new THREE.HemisphereLight(0xaaaaaa, 0x000000, .9)
+    hemisphereLight.castShadow = true;
 
     // A directional light shines from a specific direction.
     // It acts like the sun, that means that all the rays produced are parallel.
@@ -885,8 +886,33 @@ class Chunk {
         this.decorateTree();
         //this.createFireBall();
         // this.createWater2();
+        // this.createLight()
         this.createWater(); //using shader
         this.createWater3(); //using setY
+    }
+
+    createLight(){
+        shadowLight = new THREE.DirectionalLight(0xffffff, .9);
+
+        // Set the direction of the light
+        shadowLight.position.set(150, 350, 350);
+
+        // Allow shadow casting
+        shadowLight.castShadow = true;
+
+        // define the visible area of the projected shadow
+        shadowLight.shadow.camera.left = -400;
+        shadowLight.shadow.camera.right = 400;
+        shadowLight.shadow.camera.top = 400;
+        shadowLight.shadow.camera.bottom = -400;
+        shadowLight.shadow.camera.near = 1;
+        shadowLight.shadow.camera.far = 1000;
+
+        // define the resolution of the shadow; the higher the better,
+        // but also the more expensive and less performant
+        shadowLight.shadow.mapSize.width = 2048;
+        shadowLight.shadow.mapSize.height = 2048;
+        this.mesh.add(shadowLight.target)
     }
 
     getDisplacementX(){
@@ -1378,6 +1404,30 @@ class AirPlane {
         this.mesh.receiveShadow = true;
 
 
+    }
+
+    createLight(){
+        shadowLight = new THREE.DirectionalLight(0xffffff, .9);
+        // Set the direction of the light
+        shadowLight.position.set(150, 350, 350);
+
+        // Allow shadow casting
+        shadowLight.castShadow = true;
+
+        // define the visible area of the projected shadow
+        shadowLight.shadow.camera.left = -400;
+        shadowLight.shadow.camera.right = 400;
+        shadowLight.shadow.camera.top = 400;
+        shadowLight.shadow.camera.bottom = -400;
+        shadowLight.shadow.camera.near = 1;
+        shadowLight.shadow.camera.far = 1000;
+
+        // define the resolution of the shadow; the higher the better,
+        // but also the more expensive and less performant
+        shadowLight.shadow.mapSize.width = 2048;
+        shadowLight.shadow.mapSize.height = 2048;
+
+        this.mesh
     }
 
     updatePropeller() {
